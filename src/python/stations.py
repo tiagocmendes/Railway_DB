7f2ed1df8fd96d63391d57f191d777c020c43a68
tiagocmendes <tiagocmendes@ua.pt>
2019-05-31 00:51:41 +0100

    Removed src

--- src/python/stations.py
+++
@@ -1,9 +0,0 @@
-stations = open('insert_stations.txt', 'w+')
-
-stationsList = ["Lisboa Oriente", "Lisboa Santa Apolónia", "Entroncamento", "Santarém", "Caldas da Rainha", "Barreiro" , 
-"Setúbal" ,"Porto Campanhã" , "Porto São Bento" , "Gaia" , "Ovar" , "Aveiro" , "Figueira da Foz" , "Coimbra" , "Pombal" , "Tomar" , 
-"Abrantes" , "Guarda" , "Covilhã" , "Castelo Branco" , "Elvas" , "Portalegre", "Évora" , "Beja" , "Ourique" , "Lagos" , "Portimão" , 
-"Faro" , "Tavira" ,"Viana do Castelo" , "Braga" , "Guimarães" , "Régua" , "Vila Real" , "Mirandela" ]
-
-for i in range(len(stationsList)):
-    stations.write("INSERT INTO RAILWAY.STATION VALUES ({}, '{}', {});\n".format(i+1, stationsList[i], 11111))
--- src/python/trip_zone.py
+++
@@ -1,8 +0,0 @@
-stations = open('insert_tripZone.txt', 'w+')
-
-locList = ["Ribatejo", "Beira Litoral", "Beira Interior", "Alentejo", "Algarve", "Entre-Douro-e-Minho" , 
-"Trás-os-Montes"]
-prices = [3.5, 4.5, 3, 1.5, 2, 3, 3]
-
-for i in range(len(locList)):
-    stations.write("INSERT INTO RAILWAY.TRIP_ZONE VALUES ('{}', {}, {});\n".format(locList[i], 11111, prices[i]))
