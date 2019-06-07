d3fc05b1de078c00ed369adb4af30e792bce2310
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2019-06-07 17:36:07 +0100

    added delete tickets in the pr_delete_profile

--- src/sql/railway_delete_user.sql
+++ src/sql/railway_delete_user.sql
@@ -1,8 +1,10 @@
 USE p6g10;
 GO
-ALTER PROC Railway.pr_delete_profile (@user_id INT)
+ALTER PROC Railway.pr_delete_profile (	@user_id INT,
+										@nif INT)
 AS
 BEGIN
+	DELETE FROM Railway.Ticket WHERE nif = @nif;
 	DELETE FROM Railway.Passenger WHERE passenger_id = @user_id;
 	DELETE FROM Railway.Person WHERE id = @user_id;
 END
